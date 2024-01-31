5d1abeed50a0de4cc2c20df306a8fe1bb66df8ca
ManfredDietrich <orion65@bluewin.ch>
2024-01-31 22:21:58 +0100

    audio_process_task in eigenes File verschoben

---
+++ RTOS_tasks/audio_process_task.h
@@ -0,0 +1,16 @@
+#ifndef AUDIO_PROCESS_TASK_H
+#define AUDIO_PROCESS_TASK_H
+/*
+* AUDIO_PROCESS_TASK
+* Created: 31.01.2023 Manfred Dietrich
+* Description: Processing and equalizing PCM data 
+*/
+
+/*Definitions*/
+
+/*Variables*/
+
+/*Prototypes*/
+void audio_process_task(__unused void *params);
+
+#endif
